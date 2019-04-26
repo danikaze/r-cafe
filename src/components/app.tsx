2e9416e02b04a70c27c302d9924a8c63012389e6
danikaze <danikaze@gmail.com>
2019-04-26 10:41:58 +0900

    Show the menu for all the week (#4)
    * Week day visual components
    * Support loading other days' menus

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,10 @@
 // tslint:disable:no-boolean-literal-compare
 import * as React from 'react';
 import { Cafeteria, DayMenu, State } from '../def';
-import { selectTime, toggleOrderType, toggleOrderDirection, selectCafeteria } from '../store/actions/ui';
+import { selectTime, toggleOrderType, toggleOrderDirection, selectCafeteria, showDaySelection, selectDay } from '../store/actions/ui';
 import { loadCurrentCongestion } from '../store/actions/congestion';
-import { loadTodayMenu } from '../store/actions/menu';
-import { getNumericDate } from '../util/date';
+import { loadMenu } from '../store/actions/menu';
+import { getNumericDate, getWorkDays } from '../util/date';
 import { sortDishes } from '../util/sort-dishes';
 import { AppTitle } from './app-title';
 import { Congestion } from './congestion';
@@ -15,13 +15,15 @@ import { Error } from './error';
 import { DishDetails } from './dish-details';
 import { SortButton } from './sort-button';
 import { ErrorRap } from './error-rap';
+import { WeekDay } from './week-day';
+import { WeekDaySelector } from './week-day-selector';
 
 export function App({ useStateValue }) {
   const [state, dispatch] = useStateValue();
-  const { status, time, cafeteria, sortBy, sortOrder, rapAccess, apiAccess } = state as State;
+  const { status, selectingDay, day, time, cafeteria, sortBy, sortOrder, rapAccess, apiAccess } = state as State;
 
   React.useEffect(() => {
-    dispatch(loadTodayMenu());
+    dispatch(loadMenu(day));
     dispatch(loadCurrentCongestion());
 
     const congestionHandler = setInterval(() => dispatch(loadCurrentCongestion()), CONGESTION_INTERVAL);
@@ -31,7 +33,9 @@ export function App({ useStateValue }) {
     };
   }, []);
 
-  if (status === 'waiting' || status === 'loading') {
+  const dayKey = getNumericDate(day);
+  const dayStatus = status[dayKey];
+  if (dayStatus === 'waiting' || dayStatus === 'loading') {
     return <Loading />;
   }
 
@@ -39,8 +43,7 @@ export function App({ useStateValue }) {
     return <Error />;
   }
 
-  const today = getNumericDate();
-  const todayMenu = state.menus[today] as DayMenu;
+  const todayMenu = state.menus[dayKey] as DayMenu;
 
   if (!todayMenu) {
     return <Error />;
@@ -66,12 +69,34 @@ export function App({ useStateValue }) {
     dispatch(selectCafeteria(cafeteria));
   }
 
+  function handleShowDaySelection() {
+    dispatch(showDaySelection(true));
+  }
+
+  function handleDaySelection(day: Date) {
+    dispatch(loadMenu(day));
+    dispatch(selectDay(day));
+  }
+
+  function handleHideDaySelection() {
+    dispatch(showDaySelection(false));
+  }
+
+  const calendarSheet = selectingDay
+    ? <WeekDaySelector days={getWorkDays()} active={day} onClick={handleDaySelection} />
+    : (
+        <div id='day'>
+          <WeekDay day={day} onClick={handleShowDaySelection} className='active' />;
+        </div>
+      );
+
   return (
-    <>
+    <div onClick={handleHideDaySelection}>
       <div id='top-bar'>
         <AppTitle />
         {rapAccess === false ? <ErrorRap /> : null}
         <Congestion rate={congestion} />
+        {calendarSheet}
         <Time time={time} onClick={handleTimeSelect} />
         <SortButton
           type={sortBy}
@@ -88,6 +113,6 @@ export function App({ useStateValue }) {
       <div className='dishes'>
         {dishes.map((dish) => <DishDetails key={dish.id} {...dish} />)}
       </div>
-    </>
+    </div>
   );
 }
--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,9 +1,11 @@
 import { LoadMenu, UpdateMenu } from './menu';
 import { UpdateCurrentCongestion } from './congestion';
-import { SelectTime, SelectCafeteria, ToggleOrderType, ToggleOrderDirection } from './ui';
+import { SelectTime, SelectCafeteria, ToggleOrderType, ToggleOrderDirection, SelectDay, ShowDaySelection } from './ui';
 import { UpdateApiAccess, UpdateRapAccess } from './service';
 
 export type Action = SelectTime
+                   | SelectDay
+                   | ShowDaySelection
                    | SelectCafeteria
                    | LoadMenu
                    | UpdateMenu
