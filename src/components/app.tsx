213d7a90d6cd07968cc1f782f48084676cd4da0e
danikaze <danikaze@gmail.com>
2019-04-26 12:37:54 +0900

    Reset the menu scroll to the top when changing day, time or cafeteria

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,6 @@
 // tslint:disable:no-boolean-literal-compare
 import * as React from 'react';
-import { Cafeteria, DayMenu, State, Dish } from '../def';
+import { Cafeteria, DayMenu, State, Dish, Time } from '../def';
 import { selectTime, toggleOrderType, toggleOrderDirection, selectCafeteria, showDaySelection, selectDay } from '../store/actions/ui';
 import { loadCurrentCongestion } from '../store/actions/congestion';
 import { loadMenu } from '../store/actions/menu';
@@ -9,7 +9,7 @@ import { sortDishes } from '../util/sort-dishes';
 import { AppTitle } from './app-title';
 import { Congestion } from './congestion';
 import { CafeteriaList } from './cafeteria-list';
-import { Time } from './time';
+import { Time as TimeElem } from './time';
 import { Loading } from './loading';
 import { Error } from './error';
 import { DishDetails } from './dish-details';
@@ -19,7 +19,8 @@ import { WeekDay } from './week-day';
 import { WeekDaySelector } from './week-day-selector';
 
 export function App({ useStateValue }) {
-  function handleTimeSelect(time) {
+  function handleTimeSelect(time: Time) {
+    resetScroll({ time });
     dispatch(selectTime(time));
   }
 
@@ -32,6 +33,7 @@ export function App({ useStateValue }) {
   }
 
   function handleCafeteriaSelect(cafeteria) {
+    resetScroll({ cafeteria });
     dispatch(selectCafeteria(cafeteria));
   }
 
@@ -40,12 +42,21 @@ export function App({ useStateValue }) {
   }
 
   function handleDaySelection(day: Date) {
+    resetScroll({ day });
     dispatch(loadMenu(day));
     dispatch(selectDay(day));
   }
 
   function handleHideDaySelection() {
     dispatch(showDaySelection(false));
+  }
+
+  function resetScroll(changes: { time?: Time, day?: Date, cafeteria?: Cafeteria }): void {
+    if ((changes.time && (changes.time !== time))
+        || (changes.day && (changes.day.getDate() !== day.getDate()))
+        || (changes.cafeteria && (changes.cafeteria !== cafeteria))) {
+      window.scrollTo(0, 0);
+    }
   }
 
   const [state, dispatch] = useStateValue();
@@ -99,7 +110,7 @@ export function App({ useStateValue }) {
         {rapAccess === false ? <ErrorRap /> : null}
         <Congestion rate={congestion} />
         {calendarSheet}
-        <Time time={time} onClick={handleTimeSelect} />
+        <TimeElem time={time} onClick={handleTimeSelect} />
         <SortButton
           type={sortBy}
           direction={sortOrder}
